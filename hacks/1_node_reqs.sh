d234cf3980afac041cd17162b00950fda12036e9
Leo Ochoa <lochoa@redhat.com>
2021-11-17 22:03:50 +0100

    Update 1_node_reqs.sh

--- hacks/1_node_reqs.sh
+++ hacks/1_node_reqs.sh
@@ -12,17 +12,8 @@ function update_system() {
   dnf -y upgrade
 }
 
-function get_devtools() {
-  # required tools
-  dnf install -y libvirt httpd
-
-  # nice to have tools
-  dnf install -y bash-completion jq tmux vim podman skopeo
-  # TODO: install ndptool ketall
-}
-
 function download_oc_client() {
-  # TODO: download oc from scratch
+  echo "TODO: automate oc from scratch"
   cp /vagrant/binaries/oc /usr/bin/oc
 
 	oc adm --registry-config "${PULL_SECRET_JSON}" release extract \
@@ -52,21 +43,50 @@ function download_ipi_installer() {
 	sudo mv openshift-baremetal-install /usr/bin/openshift-baremetal-install
 }
 
-function disable_slaac() {
-  cp /vagrant/templates/etc/sysctl.d/ip.conf /etc/sysctl.d/ip.conf
+function download_kubectl() {
+  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/$(uname -m | sed "s/x86_64/amd64/" | sed "s/aarch64/arm64/")/kubectl"
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
+}
+
+function get_ket-all() {
+  curl -Lo ketall.gz https://github.com/corneliusweig/ketall/releases/download/v1.3.8/ketall-amd64-linux.tar.gz && \
+    gunzip ketall.gz && chmod +x ketall && mv ketall "$GOPATH"/bin/
+}
+
+function get_tooling() {
+  # required tools
+  dnf install -y libvirt httpd chrony podman dnsmasq radvd
+  download_kubectl
+  download_oc_client
+  download_ipi_installer
+
+  # nice to have tools
+  dnf install -y bash-completion jq tmux vim skopeo libndp ipmitool
+  echo "TODO: check ket-all install"
+  # get_ket-all
+}
+
+function set_kernel_flags() {
+  cp /vagrant/templates/etc/sysctl.d/ip.conf /etc/sysctl.d/99-sysctl.conf
+  sysctl -p
 }
 
 
-# ===========================
-#  1.1) Bootstrapping Phase #
-# ===========================
+# =============================================================
+# 1.1) Hardware / Baseboard Management Controller (BMC) Phase #
+# =============================================================
+# TODO: Check minimum hardware requirements
+# TODO: Install latest BIOS and driver versions
+# TODO: Clean up boot entries
+
+
+# ===========================================
+# 1.2) Operating System (OS) / Kernel Phase #
+# ===========================================
+# 1.2.1) Bastion node bootstrapping
 update_system
-get_devtools
-download_oc_client
-download_ipi_installer
+get_tooling
 
-
-# ========================
-# 1.2) OS / Kernel Phase #
-# ========================
-disable_slaac  # this is here due to a node reboot
+# 1.2.2) Kernel configurations
+set_kernel_flags
